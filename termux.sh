dbcb006ae03a7af7016392b816dd001a3cad4c79
Vatsal850 <53715295+Vatsal850@users.noreply.github.com>
2020-03-22 15:52:15 +0530

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -1,8 +1,13 @@
- apt update -y
- apt upgrade -y
- apt install x11-repo -y
- apt install tigervnc fluxbox openbox lynx -y
+ apt-get update -y
+ clear
+ apt-get upgrade -y
+ clear
+ pkg install x11-repo -y
+ clear 
+ apt-get install tigervnc fluxbox openbox lynx -y
  clear && echo "now you can start vncserver by 
       typing these commands:-
        $ export DISPLAY=":1"
-       $ vncserver-localhost "
+       $ vncserver  
+     Computer name-termux
+     Address-localhost:5901 "
